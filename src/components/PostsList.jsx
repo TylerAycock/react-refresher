d43b6bbe0a39309549d5b1135385c957d5f21e3d
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2024-01-11 15:10:50 -0800

    added useEffect and initial get request

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,12 +2,29 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import Modal from "./Modal";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const PostsList = ({ modalVisability, modalToggle }) => {
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const res = await fetch("http://localhost:8080/posts");
+      const resData = await res.json();
+      setPosts(resData.posts);
+    }
+    fetchPosts()
+  }, []);
+
   const [posts, setPosts] = useState([]);
 
   const addPostHandler = (postData) => {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     setPosts((existingPosts) => [...existingPosts, postData]);
   };
 
@@ -32,7 +49,7 @@ const PostsList = ({ modalVisability, modalToggle }) => {
       )}
       {posts.length === 0 && (
         <div className={classes.empty}>
-          <h2>There are no posts yet</h2>
+          <h2>There are no posts yet.</h2>
           <p>Start Adding some!</p>
         </div>
       )}
